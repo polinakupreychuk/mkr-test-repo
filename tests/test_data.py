fe3af4b3e8064e8babd8cb0ab6f60225f87324ab
Polina <polina_kupreychuk@knu.ua>
2024-04-05 14:56:55 +0300

    docstring added in test

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,6 +5,9 @@ from main import read_population_data, sort_by_area, sort_by_population
 
 
 def test_read_population_data():
+    """
+       Test if the read_population_data function correctly reads data from a file.
+    """
     file_name = "test_data.txt"
     with open(file_name, 'w') as file:
         file.write("Country1,100,1000\n")
@@ -15,12 +18,18 @@ def test_read_population_data():
 
 
 def test_sort_by_area():
+    """
+        Test sorting population data by area.
+    """
     population_data = [('Country2', 200.0, 2000), ('Country1', 100.0, 1000)]
     sorted_data = sort_by_area(population_data)
     assert sorted_data == [('Country1', 100.0, 1000), ('Country2', 200.0, 2000)]
 
 
 def test_sort_by_population():
+    """
+        Test sorting population data by population.
+    """
     population_data = [('Country2', 200.0, 2000), ('Country1', 100.0, 1000)]
     sorted_data = sort_by_population(population_data)
     assert sorted_data == [('Country1', 100.0, 1000), ('Country2', 200.0, 2000)]
@@ -32,10 +41,16 @@ def population_data():
 
 
 def test_sort_by_area(population_data):
+    """
+    Test sorting population data by area using fixture.
+    """
     sorted_data = sort_by_area(population_data)
     assert sorted_data == [('Country1', 100.0, 1000), ('Country2', 200.0, 2000)]
 
 
 def test_sort_by_population(population_data):
+    """
+        Test sorting population data by population using fixture.
+    """
     sorted_data = sort_by_population(population_data)
     assert sorted_data == [('Country1', 100.0, 1000), ('Country2', 200.0, 2000)]
