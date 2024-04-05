8a2d3c8c15870595e1d4bf597d55e7ad524f3b7e
Polina <polina_kupreychuk@knu.ua>
2024-04-05 14:11:51 +0300

    main.py created

--- main.py
+++ main.py
@@ -1,45 +1,30 @@
-def read_population_data(filename):
+def read_population_data(file_name):
     population_data = []
-    with open(filename, 'r') as file:
+    with open(file_name, 'r') as file:
         for line in file:
-            # Розділити рядок за комою і видалити зайві пробіли
-            country, area, population = map(str.strip, line.split(','))
-            # Додати дані про популяцію у список
+            country, area, population = line.strip().split(',')
             population_data.append((country, float(area), int(population)))
     return population_data
 
 def sort_by_area(population_data):
-    # Сортувати дані за площею
-    sorted_by_area = sorted(population_data, key=lambda x: x[1], reverse=True)
-    return sorted_by_area
+    return sorted(population_data, key=lambda x: x[1])
 
 def sort_by_population(population_data):
-    # Сортувати дані за населенням
-    sorted_by_population = sorted(population_data, key=lambda x: x[2], reverse=True)
-    return sorted_by_population
+    return sorted(population_data, key=lambda x: x[2])
 
 def main():
-    filename = input("Введіть назву файлу: ")
-    try:
-        # Зчитати дані про популяцію з файлу
-        population_data = read_population_data(filename)
-        # Відсортувати дані за площею
-        sorted_by_area = sort_by_area(population_data)
-        # Відсортувати дані за населенням
-        sorted_by_population = sort_by_population(population_data)
+    file_name = "text.txt"
+    population_data = read_population_data(file_name)
 
-        # Вивести результати
-        print("Дані, відсортовані за площею:")
-        for country, area, population in sorted_by_area:
-            print(f"{country}: площа - {area}, населення - {population}")
+    sorted_by_area = sort_by_area(population_data)
+    print("Sorted by area:")
+    for country_data in sorted_by_area:
+        print(country_data)
 
-        print("\nДані, відсортовані за населенням:")
-        for country, area, population in sorted_by_population:
-            print(f"{country}: площа - {area}, населення - {population}")
-    except FileNotFoundError:
-        print("Файл не знайдено.")
-    except Exception as e:
-        print(f"Сталася помилка: {e}")
+    sorted_by_population = sort_by_population(population_data)
+    print("\nSorted by population:")
+    for country_data in sorted_by_population:
+        print(country_data)
 
 if __name__ == "__main__":
     main()
